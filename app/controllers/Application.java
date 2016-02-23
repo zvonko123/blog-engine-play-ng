09476457c4282e7ff160172c5d86914af518fca1
Zvonimir Štimac <zvone@mbit.io>
2016-02-23 11:21:18 +0100

    configured persistence

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,35 @@
 package controllers;
 
+import com.google.common.collect.Lists;
+import models.Post;
 import play.*;
+import play.db.jpa.JPA;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.List;
+
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Blog"));
+
+        String title = "first post title";
+        String body = "first post body";
+
+        final Post post = new Post(title, body);
+        JPA.withTransaction(() -> {
+            JPA.em().persist(post);
+        });
+
+        final List<Post> posts= Lists.newArrayList();
+
+        JPA.withTransaction(() -> {
+            posts.addAll(JPA.em().createQuery("select e from Post e", Post.class).getResultList());
+        });
+
+
+        return ok(index.render(posts.get(0).toString()));
     }
 
 }
--- app/models/Post.java
+++ app/models/Post.java
@@ -11,6 +11,42 @@ import javax.persistence.*;
 
 public class Post {
 
+    public Post() {
+    }
+
+    public Post(String title, String body) {
+        this.setTitle(title);
+        this.setBody(body);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Integer getId() {
+
+        return id;
+    }
+
     @Id
     @GeneratedValue
     public Integer id;
@@ -19,6 +55,5 @@ public class Post {
     @Column
     public String body;
     //public Comment[] comments;
-    @Column
-    public DateTime timestamp;
+
 }
